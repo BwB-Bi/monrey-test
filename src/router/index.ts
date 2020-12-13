6db037b2301737a2e966e1689524a56f80986e8e
joco <13711619148@163.com>
2020-12-13 12:28:16 +0800

    添加404

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,27 +4,31 @@ import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
     {
-        path:'/',
-        redirect:'/money'
+        path: '/',
+        redirect: '/money'
     },
     {
-        path:'/money',
-        component:Money
+        path: '/money',
+        component: Money
     },
     {
-        path:'/labels',
-        component:Labels
+        path: '/labels',
+        component: Labels
     },
     {
-        path:'/statistics',
-        component:Statistics
+        path: '/statistics',
+        component: Statistics
+    },
+    {
+        path: '*',
+        component: NotFound
     }
-
 ];
 
 const router = new VueRouter({
