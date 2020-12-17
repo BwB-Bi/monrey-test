0c159ec24751d53b211cab0ff21c7c40f5f1daef
joco <13711619148@163.com>
2020-12-17 19:48:51 +0800

    封装 record 的操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,11 +18,7 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import {recordListModel} from '@/models/recordListModel';
-
-
-const recordList = recordListModel.fetch();
+import {Component} from 'vue-property-decorator';
 
 
 @Component({
@@ -30,7 +26,7 @@ const recordList = recordListModel.fetch();
 })
 export default class Money extends Vue {
   tags = window.tagList;
-  recordList: RecordItem[] = recordList;
+  recordList= window.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -46,12 +42,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record)
-  }
-
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
+   window.create(this.record)
   }
 }
 </script>
