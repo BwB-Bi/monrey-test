f0546836336007acea7c732bb63be22fce4622c4
joco <13711619148@163.com>
2020-12-17 16:32:58 +0800

    可以编辑和删除标签

--- src/views/Editlabel.vue
+++ src/views/Editlabel.vue
@@ -1,15 +1,15 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name" @update:value="update" field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -22,28 +22,45 @@ import FormItem from '@/components/Money/FormItem.vue';
 import Button from '@/components/Button.vue';
 
 @Component({
-  components:{Button,  FormItem}
+  components: {Button, FormItem}
 })
 export default class Editlabel extends Vue {
-   tag?: {id: string ; name: string} = undefined;
+  tag?: { id: string; name: string } = undefined;
+
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch();
-    const tags=tagListModel.data;
-    const tag=tags.filter(t => t.id === id)[0];
-    if(tag){
-      this.tag=tag;
-    }else{
+    const tags = tagListModel.data;
+    const tag = tags.filter(t => t.id === id)[0];
+    if (tag) {
+      this.tag = tag;
+    } else {
       this.$router.replace('/404');
     }
   }
 
+  update(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+
+  remove() {
+    if (this.tag) {
+      tagListModel.remove(this.tag.id);
+    }
+
+  }
+
+  goBack() {
+    this.$router.back();
+  }
 
 }
 </script>
 
 <style lang="scss" scoped>
-.navBar{
+.navBar {
   text-align: center;
   font-size: 16px;
   padding: 12px 16px;
@@ -52,22 +69,27 @@ export default class Editlabel extends Vue {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  >.title{
+
+  > .title {
   }
-  >.leftIcon{
-    width:24px;
-    height:24px;
+
+  > .leftIcon {
+    width: 24px;
+    height: 24px;
   }
-  >.rightIcon{
-    width:24px;
-    height:24px;
+
+  > .rightIcon {
+    width: 24px;
+    height: 24px;
   }
 
 }
+
 .form-wrapper {
   background: white;
   margin-top: 8px;
 }
+
 .button-wrapper {
   text-align: center;
   padding: 16px;
