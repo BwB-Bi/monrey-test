314bf8a599cf8715bcb66e8687eba5abbbb75ff5
joco <13711619148@163.com>
2020-12-17 20:30:26 +0800

    将所有数据操作封装在store里

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,4 +1,3 @@
-
 type RecordItem = {
     tags: string[];
     notes: string;
@@ -20,12 +19,3 @@ type TagListModel = {
     save: () => void;
 }
 
-interface Window{
-    tagList: Tag[];
-    findTag: (id: string) => Tag | undefined;
-    createTag: (name: string) => void;
-    removeTag: (id: string) => boolean;
-    updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
-    recordList: RecordItem[];
-    createRecord: (record: RecordItem) => void;
-}
