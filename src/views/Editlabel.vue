04c0579d567b540c3598999a2b3cc04e598785e9
joco <13711619148@163.com>
2020-12-19 19:55:33 +0800

    EditLabel.vue使用Vuex

--- src/views/Editlabel.vue
+++ src/views/Editlabel.vue
@@ -24,13 +24,17 @@ import Button from '@/components/Button.vue';
 
 
 @Component({
-  components: {Button, FormItem}
+  components: {Button, FormItem},
 })
 export default class EditLabel extends Vue {
-  tag?: Tag = undefined;
+
+  get tag() {
+    return this.$store.state.currentTag;
+  }
 
   created() {
-    //this.tag = {}; //store.findTag(this.$route.params.id);
+    const id = this.$route.params.id;
+    this.$store.commit('setCurrentTag', id);
     if (!this.tag) {
       this.$router.replace('/404');
     }
@@ -39,18 +43,18 @@ export default class EditLabel extends Vue {
 
   update(name: string) {
     if (this.tag) {
-     // store.updateTag(this.tag.id, name);
+      // store.updateTag(this.tag.id, name);
     }
   }
 
   remove() {
     if (this.tag) {
       //
-//      if (store.removeTag(this.tag.id)) {
-//        this.$router.back();
-//      } else {
-//        window.alert('删除失败');
-//      }
+      //      if (store.removeTag(this.tag.id)) {
+      //        this.$router.back();
+      //      } else {
+      //        window.alert('删除失败');
+      //      }
     }
   }
 
