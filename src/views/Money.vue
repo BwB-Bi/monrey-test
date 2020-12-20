c31cf10d320d974a93f3914bb46cf08c7876248d
joco <13711619148@163.com>
2020-12-20 16:09:22 +0800

    修复一进页面就弹出四个提示的bug

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -6,10 +6,10 @@
     <div class="notes">
       <FormItem field-name="备注"
                 placeholder="在这里输入备注"
-                @update:value="onUpdateNotes"
+                :value.sync="record.notes"
       />
     </div>
-    <Tags/>
+    <Tags @update:value="record.tags=$event"/>
   </Layout>
 </template>
 
@@ -21,6 +21,7 @@ import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
+
 @Component({
   components: {Tabs, Tags, FormItem, NumberPad},
 })
@@ -28,18 +29,29 @@ export default class Money extends Vue {
   get recordList() {
     return this.$store.state.recordList;
   }
+
   recordTypeList = recordTypeList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
+
   created() {
     this.$store.commit('fetchRecords');
   }
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
+
   saveRecord() {
+    if (!this.record.tags || this.record.tags.length === 0) {
+      return window.alert('请至少选择一个标签');
+    }
     this.$store.commit('createRecord', this.record);
+    if (this.$store.state.createRecordError === null) {
+      window.alert('已保存');
+      this.record.notes = '';
+    }
   }
 }
 </script>
@@ -49,6 +61,7 @@ export default class Money extends Vue {
   display: flex;
   flex-direction: column-reverse;
 }
+
 .notes {
   padding: 12px 0;
 }
